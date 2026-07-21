ecf47fd4923d0bdc6a021c626c1375733accbaef
shanusrigowtham <shanusrigowtham17@gmail.com>
2026-07-21 15:09:21 +0530

    Update main.py

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:3000",
-        "https://your-new-frontend.vercel.app"  # <-- update to your actual frontend URL
+        "https://front-end-omega-swart.vercel.app"  # <-- update to your actual frontend URL
     ],
     allow_credentials=True,
     allow_methods=["*"],
